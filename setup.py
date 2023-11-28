97e565e5f5c3d55f156f46d7addb13e3c2988ac4
LokeshDondapati <ldondapa@mail.yu.edu>
2023-11-28 16:56:11 -0500

    commit

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@
 from distutils.core import setup
 
 setup(
-    name='Web_data_analysis',
-    version='0.2.1',
+    name='webanalysis',
+    version='0.0.1',
     description='Data analysis.',
     author='Lokesh Dondapati',
     author_email='ldondapa@mail.yu.edu',
     license='MIT',
-    url='https://github.com/stefmolin/stock-analysis',
-    packages=['Web_data_analysis'],
+    url='https://github.com/LokeshDondapati/Web-Data-Analysis',
+    packages=['analysis'],
     install_requires=[
         'matplotlib>=3.0.2',
         'numpy>=1.15.2',
@@ -19,8 +19,7 @@ setup(
         'black>=23.11.0,'
         'lxml>=4.9.3',
         'selenium>=4.15.12',
-        'beautifulsoup4>=04.12.2'
+        'beautifulsoup4>=4.12.2'
     ],
 )
-
 
